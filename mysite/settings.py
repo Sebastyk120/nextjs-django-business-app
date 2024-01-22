270f5aecd01c99b60cad21a9e8a439ae266c34ea
sebastyk120@gmail.com <sebastyk120@gmail.com>
2024-01-22 14:33:42 -0500

    4

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,7 +150,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
 
-ALLOWED_HOSTS = ["django-server-production-8f77.up.railway.app/"]
+ALLOWED_HOSTS = [
+    'django-server-production-8f77.up.railway.app',
+    # Agrega otros dominios si es necesario
+]
 CSRF_TRUSTED_ORIGINS = ["https://django-server-production-8f77.up.railway.app/"]
 CSRF_ALLOWED_ORIGINS = ["https://django-server-production-8f77.up.railway.app/"]
 CORS_ORIGINS_WHITELIST = ["https://django-server-production-8f77.up.railway.app/"]
