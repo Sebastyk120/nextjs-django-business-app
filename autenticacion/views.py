25c476beb8e2c98e9a4d791b2a29d48e9141efe1
Sebastian <sebastyk120@gmail.com>
2024-02-15 23:56:53 -0500

    Reestablecer contraseña 3

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -83,5 +83,5 @@ class CustomPasswordResetView(PasswordResetView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['header_text'] = "Administración Heavens Fruits"
+        context['site_name'] = "Administración Heavens Fruits"
         return context
