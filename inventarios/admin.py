92cef75bee5ffd9f7ed0b182a5e73df28216142c
Sebastian <sebastyk120@gmail.com>
2025-01-16 13:41:08 +0100

    Mejoras admin e inventarios bodegas.

--- inventarios/admin.py
+++ inventarios/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin
+from django.utils.html import format_html
 from .models import Bodega, Item, Movimiento, Inventario, Proveedor
 from .resources import MovimientoResource, ItemResource, BodegaResource, ProveedorResource, UserResource, GroupResource
 from import_export.admin import ImportExportModelAdmin
@@ -16,17 +16,88 @@ class MyModelAdmin(ImportExportModelAdmin):
     import_error_display = ("message", "row", "traceback")
     resource_class = ItemResource
 
+    # Campos para mostrar en modo tabla
+    list_display = (
+        "numero_item",
+        "cantidad_cajas",
+        "tipo_documento",
+        "documento",
+        "bodega",
+        "proveedor",
+        "fecha_movimiento",
+        "propiedad",
+        "user",
+        "observaciones_display"
+    )
+    # Campos para búsqueda
+    search_fields = ("numero_item__nombre",)
+    # Filtros laterales
+    list_filter = (
+        "bodega",
+        "proveedor",
+        "propiedad"
+    )
+
+    # Orden predeterminado
+    ordering = ("-fecha_movimiento",)
+
+    # Mostrar observaciones con un límite de caracteres
+    def observaciones_display(self, obj):
+        if obj.observaciones:
+            return format_html(f"{obj.observaciones[:50]}{'...' if len(obj.observaciones) > 50 else ''}")
+        return "-"
+    observaciones_display.short_description = "Observaciones"
+
 
 @admin.register(Movimiento)
-class MyModelAdmin(ImportExportModelAdmin):
-    import_error_display = ("message", "row", "traceback")
-    resource_class = MovimientoResource
+class MovimientoAdmin(admin.ModelAdmin):
+    # Campos para mostrar en modo tabla
+    list_display = (
+        "item_historico",
+        "cantidad_cajas_h",
+        "bodega",
+        "propiedad",
+        "fecha_movimiento",
+        "user",
+        "observaciones_display",
+        "fecha",
+    )
+
+    # Campos para búsqueda
+    search_fields = ("item_historico", "bodega__nombre", "propiedad", "user__username")
+
+    # Filtros laterales
+    list_filter = (
+        "bodega",
+        "propiedad",
+        "user",
+    )
+
+    # Orden predeterminado
+    ordering = ("-fecha",)
+
+    def observaciones_display(self, obj):
+        if obj.observaciones:
+            return f"{obj.observaciones[:50]}{'...' if len(obj.observaciones) > 50 else ''}"
+        return "-"
+    observaciones_display.short_description = "Observaciones"
 
 
 @admin.register(Bodega)
-class MyModelAdmin(ImportExportModelAdmin):
-    import_error_display = ("message", "row", "traceback")
-    resource_class = BodegaResource
+class BodegaAdmin(admin.ModelAdmin):
+    # Campos para mostrar en modo tabla
+    list_display = ("nombre", "exportador_display")
+
+    # Campos para búsqueda
+    search_fields = ("nombre", "exportador__nombre")  # Reemplaza "nombre_campo_relacionado" por el campo del modelo Exportador que identifica al exportador.
+
+    # Orden predeterminado (ya definido en el modelo, pero puedes especificarlo también aquí)
+    ordering = ("nombre",)
+    search_help_text = "Buscar por nombre del exportador"
+
+    def exportador_display(self, obj):
+        return obj.exportador  # Este devuelve la representación `__str__` del modelo Exportador.
+    exportador_display.short_description = "Exportador"
 
 
 @admin.register(Proveedor)
@@ -68,8 +139,9 @@ class InventarioAdmin(admin.ModelAdmin):
         'ventas', 
         'venta_contenedor'
     )
-    list_filter = ('numero_item',)  # Agrega un filtro por referencia
+    list_filter = ('numero_item__exportador__nombre',)  # Filtro exportador
     search_fields = ('numero_item__nombre', 'numero_item__exportador__nombre')  # Búsqueda por campos relacionados
+    search_help_text = 'Buscar por nombre del item o nombre del exportador'
     ordering = ('numero_item',)  # Ordena por el campo definido en Meta
 
 # Registra el modelo y su configuración
