2af145f8595cb8a9d62ebd7bd6d4f9d768e8ddb5
Sebastian <sebastyk120@gmail.com>
2025-08-28 13:22:19 +0200

    redeploy

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# These settings will be handled by Unfold, but we'll keep them for compatibility
+# These settings will be handled by Unfold, but we'll keep them for compatibility hol
 admin.site.site_header = "Administración Heavens Fruits"
 admin.site.site_title = "Administración Heavens"
 admin.site.index_title = "Bienvenido al Portal de Administración Heavens"
