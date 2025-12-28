52c632a99e8cf7361a0605725be2e92c51c6127d
Sebastian <sebastyk120@gmail.com>
2025-12-28 14:13:12 +0100

    feat: Add initial Django admin configurations for inventory models, including import/export functionality and custom site settings.

--- inventarios/admin.py
+++ inventarios/admin.py
@@ -135,7 +135,7 @@ class UserAdmin(ModelAdmin, ImportExportModelAdmin, BaseUserAdmin):
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
 
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
